37c823f2ec56e0566e5f3d560f4d51a1f2ff7b6e
Witold Kupś <witold.kups@interlan.pl>
2022-11-07 05:44:27 +0100

    initial commit

---
+++ apps/impl-dep/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'impl-dep-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'impl-dep';
+}
